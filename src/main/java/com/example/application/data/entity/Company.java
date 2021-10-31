d993f0756aa9cdff1bc72d7686a2fdc1e51aa62b
drovocek <mycodepg@gmail.com>
2021-10-31 17:47:20 +0300

    starter done

--- src/main/java/com/example/application/data/entity/Company.java
+++ src/main/java/com/example/application/data/entity/Company.java
@@ -1,17 +1,20 @@
 package com.example.application.data.entity;
 
-import java.util.LinkedList;
-import java.util.List;
+import com.example.application.data.AbstractEntity;
+import lombok.Data;
+import org.hibernate.annotations.Formula;
 
 import javax.annotation.Nullable;
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 import javax.validation.constraints.NotBlank;
+import java.util.LinkedList;
+import java.util.List;
 
-import com.example.application.data.AbstractEntity;
-
+@Data
 @Entity
 public class Company extends AbstractEntity {
+
     @NotBlank
     private String name;
 
@@ -19,19 +22,6 @@ public class Company extends AbstractEntity {
     @Nullable
     private List<Contact> employees = new LinkedList<>();
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public List<Contact> getEmployees() {
-        return employees;
-    }
-
-    public void setEmployees(List<Contact> employees) {
-        this.employees = employees;
-    }
+    @Formula("(select count(c.id) from Contact c where c.company_id = id)")
+    private int employeeCount;
 }
