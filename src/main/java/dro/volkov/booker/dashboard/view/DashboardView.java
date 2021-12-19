2ff035a03dbf6f36d45bffcdb73579b713669902
drovocek <mycodepg@gmail.com>
2021-12-19 09:20:32 +0300

    fix board

--- src/main/java/dro/volkov/booker/dashboard/view/DashboardView.java
+++ src/main/java/dro/volkov/booker/dashboard/view/DashboardView.java
@@ -1,46 +1,43 @@
 package dro.volkov.booker.dashboard.view;
 
 import com.github.appreciated.apexcharts.config.chart.Type;
+import com.vaadin.flow.component.accordion.Accordion;
+import com.vaadin.flow.component.listbox.MultiSelectListBox;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import dro.volkov.booker.MainLayout;
 import dro.volkov.booker.dashboard.ChartNode;
-import org.vaadin.gatanaso.MultiselectComboBox;
+import lombok.RequiredArgsConstructor;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.security.PermitAll;
 import java.util.List;
 import java.util.Optional;
-import java.util.Set;
 
-//@RequiredArgsConstructor
+@RequiredArgsConstructor
 @PermitAll
 @Route(value = "dashboard", layout = MainLayout.class)
 @PageTitle("Dashboard | Booker")
 public class DashboardView extends VerticalLayout {
 
-    private List<ChartNode> charts;
+    private final List<ChartNode> charts;
 
     @PostConstruct
     private void initView() {
-        MultiselectComboBox<Type> typeSelector = new MultiselectComboBox<>("Type");
-        typeSelector.setItems(Type.values());
-        typeSelector.setItemLabelGenerator(Type::name);
-        typeSelector.addSelectionListener(event -> {
-            event.getAddedSelection()
-                    .forEach(type ->
-                            getChart(type)
-                                    .ifPresentOrElse(this::add, () -> hasNoType(type)));
-
-            Set<Type> removed = event.getRemovedSelection();
-            getChildren()
-                    .filter(component -> component instanceof ChartNode)
-                    .map(component -> (ChartNode) component)
-                    .filter(chartNode -> removed.contains(chartNode.getType()))
-                    .forEach(this::remove);
+        Accordion accordion = new Accordion();
+        MultiSelectListBox<Type> listBox = new MultiSelectListBox<>();
+        listBox.setItems(Type.values());
+        listBox.addSelectionListener(event -> {
+            event.getRemovedSelection().forEach(type -> getChart(type)
+                    .ifPresentOrElse(this::remove, () -> hasNoType(type)));
+            event.getAddedSelection().forEach(type -> getChart(type)
+                    .ifPresentOrElse(this::add, () -> hasNoType(type)));
         });
-        add(typeSelector);
+
+        accordion.add("Charts", listBox);
+
+        add(accordion);
     }
 
     private void hasNoType(Type type) {
